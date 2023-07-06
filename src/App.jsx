c4168951ac08d366e989956dec37168a445b8516
Nassy Kova <mailtosummer@gmail.com>
2023-07-06 17:01:39 +1000

    Change to scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Navbar from "./components/Navbar";
 import Project from "./components/Projects";
 import data from "./projectsData";
 import Footer from "./components/Footer";
+import "./styles.scss";
 
-import "./styles.css";
 function App() {
     const projects = data.map((item) => {
         return <Project key={item.key} {...item} />;
